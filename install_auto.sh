176e7af42771a5649b009272e44f571196d9ded5
Vladko915 <88051272+Vladko915@users.noreply.github.com>
2022-09-01 13:50:51 +0300

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y mc curl jq php7.4 wireguard \
+apt-get install -y mc curl jq pup php7.4 wireguard \
 cmatrix okular dos2unix unar zip mailutils \
 sendemail apt-transport-https sudo screen \
 git composer policykit-1-gnome timeshift \
