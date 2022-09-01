bb9a7f69f8b120657b029aba1b056e627d1b801b
Vladko915 <88051272+Vladko915@users.noreply.github.com>
2022-09-01 10:30:40 +0300

    Create install_auto.sh

---
+++ install_auto.sh
@@ -0,0 +1,7 @@
+apt-get install -y mc curl jq php7.4 wireguard \
+cmatrix okular dos2unix unar zip mailutils \
+sendemail apt-transport-https sudo screen \
+git composer policykit-1-gnome timeshift \
+nodejs npm apt-transport-https coreutils \
+gconf-service gconf2-common libgconf-2-4 gdebi \
+pulseaudio-equalizer pavucontrol bc sl snapd cowsay \
